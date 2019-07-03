8e657a78a38ce2b35f9e16bf6bb6b4120e75a691
Mark Fitzpatrick <markfitz815@gmail.com>
2019-07-03 17:23:16 +0100

    style header and add food-add component to home

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <header class="fd-nav">
-      <div class="fd-nav-title">Food Diary</div>
-      <div class="fd-nav-links">
-        <router-link to="/">Home</router-link> 
-        <router-link to="/about">About</router-link>
+      <div class="fd-nav__title">Food Diary</div>
+      <div class="fd-nav__links">
+        <router-link class="fd-nav__links__link" to="/">Home</router-link> 
+        <router-link class="fd-nav__links__link" to="/about">About</router-link>
       </div>
     </header>
     <router-view/>
@@ -19,13 +19,62 @@
 
 <style lang="scss">
 
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+
 body, html {
   font-family: Arial, Helvetica, sans-serif;
 }
 
+body {
+  font-size: 62.5%;
+}
+
 a {
   text-decoration: none;
-  color: black;
+}
+
+.fd-nav {
+  border-bottom: 1px solid #123752;
+  display: flex;
+  align-items: center;
+  background-color: #29526D;
+  color: rgb(245, 245, 245);
+}
+
+.fd-nav__title {
+  font-size: 1.8rem;
+  flex: 1;
+  
+  padding: .2em 1em;
+}
+
+.fd-nav__links {
+  display: flex;
+  
+  flex: 1;
+  
+}
+
+.fd-nav__links__link {
+  padding: 0 .8rem;
+  color: rgb(230, 230, 230);
+  font-size: 1.3rem;
+  transform: scale(1);
+  transition: transform ease-in .3s;
+}
+
+.fd-nav__links__link:hover {
+  transition: transform ease-in .3s;
+  transform: scale(1.2);
+  color: white;
+}
+
+@keyframes fd-nav-pulse {
+  0% {}
 }
 
 </style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="home">
+    <main class="content">
+      <add-food></add-food>
+    </main>
     
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import AddFood from '@/components/AddFood.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    AddFood
   }
 }
 </script>
