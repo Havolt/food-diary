9f0e19887c48e7126f206d04630fbe2e7f8808ef
Mark Fitzpatrick <markfitz815@gmail.com>
2019-07-03 15:54:32 +0100

    init vue setup

---
+++ app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <header class="fd-nav">
+      <div class="fd-nav-title">Food Diary</div>
+      <div class="fd-nav-links">
+        <router-link to="/">Home</router-link> 
+        <router-link to="/about">About</router-link>
+      </div>
+    </header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+
+
+</script>
+
+<style lang="scss">
+
+body, html {
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+a {
+  text-decoration: none;
+  color: black;
+}
+
+</style>
